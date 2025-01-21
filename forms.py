d5cfc7433497cb7c70f4445dfbbecb5e83bbed54
Saimonaligok <saimonaarmen@gmail.com>
2025-01-21 11:39:13 +0700

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-class BookForm(FlaskForm):
-    judul = StringField('Judul Buku', validators=[DataRequired(), Length(max=100)])
-    pengarang = StringField('Pengarang', validators=[DataRequired(), Length(max=100)])
-    tahun = IntegerField('Tahun Terbit', validators=[DataRequired()])
-    submit = SubmitField('Simpan')
